bfea3d5d908816fa2f212fe462f4d07bc7daa393
ax-ross <ax-ross@yandex.ru>
2022-04-18 14:59:44 +0300

    implemented TSingleton

--- shop/vendor/axross/TSingleton.php
+++ shop/vendor/axross/TSingleton.php
@@ -2,6 +2,16 @@
 
 namespace axross;
 
-trait TSingleton{
+trait TSingleton
+{
+    private static ?self $instance = null;
 
+    private function __construct()
+    {
+    }
+
+    public static function getInstance(): static
+    {
+        return static::$instance ?? static::$instance = new static();
+    }
 }
