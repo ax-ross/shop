0882ebfd99d71bf28e326eb5afb7f9c06ec0d6ef
ax-ross <ax-ross@yandex.ru>
2022-04-22 10:15:21 +0300

    added base class for Model

---
+++ shop/vendor/axross/Model.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace axross;
+
+
+abstract class model
+{
+
+    public array $attributes = [];
+    public array $errors = [];
+    public array $rules = [];
+    public array $labels = [];
+
+    public function __construct()
+    {
+        Db::getInstance();
+    }
+
+}
