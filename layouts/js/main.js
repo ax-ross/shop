c93225279cc54e9740e0015aac7952fad0f0d3f8
ax-ross <ax-ross@yandex.ru>
2022-04-23 10:30:36 +0300

    completed header-top

---
+++ layouts/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    $('.open-search').click(function(e) {
+        e.preventDefault();
+        $('#search').addClass('active');
+    });
+
+    $('.close-search').click(function() {
+        $('#search').removeClass('active');
+    });
+
+})
