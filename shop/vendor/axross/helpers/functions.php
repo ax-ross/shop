609555ae44e30b4044aae838a8e57ab7868d91a1
ax-ross <ax-ross@yandex.ru>
2022-04-26 23:06:48 +0300

    added help function redirect

--- shop/vendor/axross/helpers/functions.php
+++ shop/vendor/axross/helpers/functions.php
@@ -5,3 +5,11 @@ function debug($data, $die = false)
     echo '<pre>' . print_r($data, true) . '<pre>';
     if ($die) die();
 }
+
+function redirect($http = false)
+{   
+    if ($http) $redirect = $http;
+    else $redirect = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : PATH;
+    header("Location: $redirect");
+    die;
+}
