a7ebadd793d72ddae961fdf5c4de6ae948568241
ax-ross <ax-ross@yandex.ru>
2022-04-28 22:17:51 +0300

    added function helper function base_url

--- shop/app/views/parts/header.php
+++ shop/app/views/parts/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <base href="/">
+    <base href="<?=base_url()?>">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -64,7 +64,7 @@
             <div class="container">
                 <nav class="navbar navbar-expand-lg navbar-light">
                     <div class="container-fluid p-0">
-                        <a class="navbar-brand" href="/"><?= \axross\App::$app->getProperty('site_name') ?></a>
+                        <a class="navbar-brand" href="<?= base_url() ?>"><?= \axross\App::$app->getProperty('site_name') ?></a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
--- shop/vendor/axross/helpers/functions.php
+++ shop/vendor/axross/helpers/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+use axross\App;
+
 function debug($data, $die = false)
 {
     echo '<pre>' . print_r($data, true) . '<pre>';
@@ -12,4 +14,9 @@ function redirect($http = false)
     else $redirect = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : PATH;
     header("Location: $redirect");
     die;
+}
+
+function base_url()
+{
+    return PATH . '/' . (App::$app->getProperty('lang') ? App::$app->getProperty('lang') . '/' : '');
 }
