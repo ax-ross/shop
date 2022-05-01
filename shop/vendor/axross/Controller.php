b681bdfc40d081e7c862333d23a8d403133e0031
ax-ross <ax-ross@yandex.ru>
2022-05-01 19:38:30 +0300

    added check function isAjax

--- shop/vendor/axross/Controller.php
+++ shop/vendor/axross/Controller.php
@@ -41,4 +41,9 @@ abstract class controller
         ];
     }
 
+    public function isAjax(): bool
+    {
+        return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest';
+    }
+
 }
