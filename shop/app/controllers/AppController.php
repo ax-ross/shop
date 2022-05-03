347fb95f90c221ff4df580c2032b8ed8d95ead90
ax-ross <ax-ross@yandex.ru>
2022-05-03 23:18:42 +0300

    now categories are received in App Controller and stored in the container

--- shop/app/controllers/AppController.php
+++ shop/app/controllers/AppController.php
@@ -6,6 +6,7 @@ use app\models\AppModel;
 use app\widgets\language\Language;
 use axross\App;
 use axross\Controller;
+use RedBeanPHP\R;
 
 class AppController extends Controller
 {
@@ -18,6 +19,9 @@ class AppController extends Controller
         $languages = App::$app->setProperty('languages', Language::getLanguages());
         $lang = App::$app->setProperty('language', Language::getLanguage($languages));
         \axross\Language::load($lang['code'], $this->route);
+
+        $categories = R::getAssoc("SELECT c.*, cd.* FROM category c JOIN category_description cd ON c.id = cd.category_id WHERE cd.language_id = ?", [$lang['id']]);
+        App::$app->setProperty("categories_{$lang['code']}", $categories);
     }
 
 }
