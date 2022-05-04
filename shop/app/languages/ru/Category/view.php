ff268b6ef8926a493785ff33343ba82afe177731
ax-ross <ax-ross@yandex.ru>
2022-05-04 23:30:34 +0300

    added translating for category page

---
+++ shop/app/languages/ru/Category/view.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'category_view_no_products' => 'В этой категории товаров пока нет ...',
+    'category_view_home_breadcrumbs' => '<i class="bi bi-house-door-fill"></i>',
+    'category_view_total_pagination' => 'товара(ов) из',
+    'category_view_sort' => 'Сортировка',
+    'category_view_sort_by_default' => 'По умолчанию',
+    'category_view_sort_title_asc' => 'Название (А - Я)',
+    'category_view_sort_title_desc' => 'Название (Я - А)',
+    'category_view_sort_price_asc' => 'Цена (дешевле > дороже)',
+    'category_view_sort_price_desc' => 'Цена (дороже > дешевле)',
+];
