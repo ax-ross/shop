ccc65d44c1f97cdbb06aedc1b717d51a3e0621c3
ax-ross <ax-ross@yandex.ru>
2022-05-04 12:57:39 +0300

    refactoring 404 page

--- shop/public/errors/404.php
+++ shop/public/errors/404.php
@@ -4,32 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?= PATH ?>/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= PATH ?>/assets/css/style.css">
     <title>404 Page Not Found</title>
 </head>
 
-<style>
-    body {
-    background: #dedede;
-}
-    .page-wrap {
-        min-height: 100vh;
-    }
-</style>
 
 <body>
-    
-</body>
-</html>
 <div class="page-wrap d-flex flex-row align-items-center">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-12 text-center">
+            <div class="col-md-12 text-center not-found">
                 <span class="display-1 d-block">404</span>
-                <div class="mb-4 lead">The page you are looking for was not found.</div>
-                <a href="<?=PATH;?>" class="btn btn-link">Back to Home</a>
+                <div class="mb-4 lead">page not found</div>
+                <a href="<?= PATH ?>"><button type="button" class="btn btn-404-home">Back to Home</button></a>
             </div>
         </div>
     </div>
 </div>
+</body>
+
+<script src="<?= PATH ?>/assets/js/bootstrap.bundle.min.js"></script>
+<script src="<?= PATH ?>/assets/js/main.js"></script>
+</html>
+
