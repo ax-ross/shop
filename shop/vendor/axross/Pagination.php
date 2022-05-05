993a727e59e67b91f5b2b3ea1dd2a2c7c95bb500
ax-ross <ax-ross@yandex.ru>
2022-05-05 12:51:24 +0300

    added base Pagination class

---
+++ shop/vendor/axross/Pagination.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace axross;
+
+
+class Pagination
+{
+
+
+    public $currentPage;
+    public $perpage;
+    public $total;
+    public $countPages;
+    public $uri;
+
+    public function __construct($page, $perpage, $total)
+    {
+        $this->perpage = $perpage;
+        $this->total = $total;
+        $this->countPages = $this->getCountPages();
+        $this->currentPage = $this->getCurrentPage($page);
+        $this->uri = $this->getParams();
+    }
+
+    protected function getHtml()
+    {
+        $back = null;
+        $forward = null;
+        $startpage = null;
+        $endpage = null;
+        $page2left = null;
+        $page1left = null;
+        $page2right = null;
+        $page1right = null;
+
+        // $back
+        if ($this->currentPage > 1) {
+            $back = "<li class='page-item'><a class='page-link' href='" . $this->getLink($this->currentPage - 1) . "'>&lt;</a></li>";
+        }
+
+        // $forward
+        if ($this->currentPage < $this->countPages) {
+            $forward = "<li class='page-item'><a class='page-link' href='" . $this->getLink($this->currentPage + 1) . "'>&gt;</a></li>";
+        }
+
+        // $startpage
+        if ($this->currentPage > 3) {
+            $startpage = "<li class='page-item'><a class='page-link' href='" . $this->getLink(1) . "'>&laquo;</a></li>";
+        }
+
+        // $endpage
+        if ($this->currentPage < ($this->countPages - 2)) {
+            $endpage = "<li class='page-item'><a class='page-link' href='" . $this->getLink($this->countPages) . "'>&raquo;</a></li>";
+        }
+
+        // $page2left
+        if ($this->currentPage > 2) {
+            $page2left = "<li class='page-item'><a class='page-link' href='" . $this->getLink($this->currentPage - 2) . "'>" . ($this->currentPage - 2) . "</a></li>";
+        }
+
+        // $page1left
+        if ($this->currentPage > 1) {
+            $page1left = "<li class='page-item'><a class='page-link' href='" . $this->getLink($this->currentPage - 1) . "'>" . ($this->currentPage - 1) . "</a></li>";
+        }
+
+        // $page2right
+        if ($this->currentPage + 2 <= $this->countPages) {
+            $page2right = "<li class='page-item'><a class='page-link' href='" . $this->getLink($this->currentPage + 2) . "'>" . ($this->currentPage + 2) . "</a></li>";
+        }
+
+        // $page1right
+        if ($this->currentPage + 1 <= $this->countPages) {
+            $page1right = "<li class='page-item'><a class='page-link' href='" . $this->getLink($this->currentPage + 1) . "'>" . ($this->currentPage + 1) . "</a></li>";
+        }
+
+        return '<nav aria-label="Page navigation example"><ul class="pagination">' . $startpage . $back . $page2left . $page1left . '<li class="page-item active"><a class="page-link">' . $this->currentPage . '</a></li>' . $page1right . $page2right . $forward . $endpage . '</ul></nav>';
+    }
+
+    protected function getCountPages()
+    {
+        return ceil($this->total / $this->perpage) ?: 1;
+    }
+
+    protected function getCurrentPage($page)
+    {
+        if (!$page || $page < 1) {
+            $page = 1;
+        }
+        if ($page > $this->countPages) {
+            $page = $this->countPages;
+        }
+        return $page;
+    }
+
+    protected function getParams()
+    {
+        $url = $_SERVER['REQUEST_URI'];
+        $url = explode('?', $url);
+        $uri = $url[0];
+        if (isset($url[1]) && $url[1] != '') {
+            $uri .= '?';
+            $params = explode('&', $url[1]);
+            foreach ($params as $param) {
+                if (!preg_match("#page=#", $param)) {
+                    $uri .= "{$param}&";
+                }
+            }
+        }
+        return $uri;
+    }
+
+    public function getStart()
+    {
+        return ($this->currentPage - 1) * $this->perpage;
+    }
+
+    protected function getLink($page)
+    {
+        if ($page == 1) {
+            return rtrim($this->uri, '?&');
+        }
+        if (str_contains($this->uri, '&')) {
+            return "{$this->uri}page={$page}";
+        } else {
+            if (str_contains($this->uri, '?')) {
+                return "{$this->uri}page={$page}";
+            } else {
+                return "{$this->uri}?page={$page}";
+            }
+        }
+    }
+
+    public function __toString()
+    {
+        return $this->getHtml();
+    }
+}
