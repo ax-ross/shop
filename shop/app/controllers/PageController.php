16a2bd3a1e1925c586904200eb642f4f980a68ab
ax-ross <ax-ross@yandex.ru>
2022-05-07 19:35:05 +0300

    added route, controller, model and view for page

--- shop/app/controllers/PageController.php
+++ shop/app/controllers/PageController.php
@@ -2,13 +2,21 @@
 
 namespace app\controllers;
 
-use axross\Controller;
+use axross\App;
 
-class PageController extends Controller
+class PageController extends AppController
 {
 
     public function viewAction() 
     {
-        echo __METHOD__;
+        $lang = App::$app->getProperty('language');
+        $page = $this->model->get_page($this->route['slug'], $lang);
+        
+        if (!$page) {
+            $this->error_404();
+            return;
+        }
+        $this->setMeta($page['title'], $page['description'], $page['keywords']);
+        $this->set(compact('page'));
     }
 }
--- shop/config/routes.php
+++ shop/config/routes.php
@@ -12,6 +12,7 @@ Router::add('#^(?<lang>[a-z]+)?/?product/(?<slug>[a-z0-9-]+)/?$#', ['controller'
 Router::add('#^(?<lang>[a-z]+)?/?category/(?<slug>[a-z0-9-]+)/?$#', ['controller' => 'Category', 'action' => 'view']);
 Router::add('#^(?<lang>[a-z]+)?/?search/?$#', ['controller' => 'Search', 'action' => 'index']);
 Router::add('#^(?<lang>[a-z]+)?/?wishlist/?$#', ['controller' => 'Wishlist', 'action' => 'index']);
+Router::add('#^(?<lang>[a-z]+)?/?page/(?<slug>[a-z0-9-]+)/?$#', ['controller' => 'Page', 'action' => 'view']);
 
 Router::add('#^(?<lang>[a-z]+)?/?$#', ['controller' => 'Main']);
 
