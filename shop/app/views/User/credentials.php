8fdd87a0d41c6a5d69e438ae614d48efdf3026c1
ax-ross <ax-ross@yandex.ru>
2022-05-16 06:10:36 +0300

    added view for credentials of user

---
+++ shop/app/views/User/credentials.php
@@ -0,0 +1,60 @@
+<div class="container">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb  bg-light p-2">
+            <li class="breadcrumb-item"><a href="<?= base_url() ?>"><i class="bi bi-house-door-fill"></i></a></li>
+            <li class="breadcrumb-item"><a href="user/cabinet"><?php et('tpl_cabinet'); ?></a></li>
+            <li class="breadcrumb-item active"><?php et('user_credentials_title') ?></li>
+        </ol>
+    </nav>
+</div>
+
+<div class="container py-3">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="section-title"><?php et('user_credentials_title'); ?></h1>
+        </div>
+
+        <?php $this->getPart('parts/cabinet_sidebar'); ?>
+
+        <div class="col-md-9 order-md-1">
+            <form class="row g-3" method="POST">
+
+                <div class="col-md-6 offset-md-3">
+                    <div class="form-floating mb-3">
+                        <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com" value="<?= htmlspecialchars($_SESSION['user']['email']); ?>" disabled>
+                        <label for="email"><?php et('tpl_signup_email_input'); ?></label>
+                    </div>
+                </div>
+
+                <div class="col-md-6 offset-md-3">
+                    <div class="form-floating mb-3">
+                        <input type="password" name="password" class="form-control" id="password" placeholder="password">
+                        <label for="password"><?php et('tpl_signup_password_input'); ?></label>
+                    </div>
+                </div>
+
+                <div class="col-md-6 offset-md-3">
+                    <div class="form-floating mb-3">
+                        <input type="text" name="name" class="form-control" id="name" placeholder="Name" value="<?= htmlspecialchars($_SESSION['user']['name']); ?>">
+                        <label for="name"><?php et('tpl_signup_name_input'); ?></label>
+                    </div>
+                </div>
+
+                <div class="col-md-6 offset-md-3">
+                    <div class="form-floating mb-3">
+                        <input type="text" name="address" class="form-control" id="address" placeholder="Address" value="<?= htmlspecialchars($_SESSION['user']['address']); ?>" required>
+                        <label for="address"><?php et('tpl_signup_address_input'); ?></label>
+                    </div>
+                </div>
+
+                <div class="col-md-6 offset-md-3">
+                    <button type="submit" class="btn">
+                        <?php et('user_credentials_save_btn'); ?>
+                    </button>
+                </div>
+
+            </form>
+        </div>
+
+    </div>
+</div>
