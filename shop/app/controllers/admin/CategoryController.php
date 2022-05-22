cea4b4c930eb6fe709c42d395ff48d7e845ddf20
ax-ross <ax-ross@yandex.ru>
2022-05-22 10:38:01 +0300

    implemented adding category

--- shop/app/controllers/admin/CategoryController.php
+++ shop/app/controllers/admin/CategoryController.php
@@ -26,7 +26,11 @@ class CategoryController extends AppController
     {
         if (!empty($_POST)) {
             if ($this->model->category_validate()) {
-                $_SESSION['success'] = 'Категория сохранена';
+                if ($this->model->save_category()) {
+                    $_SESSION['success'] = 'Категория сохранена';
+                } else {
+                    $_SESSION['errors'] = 'Ошибка!';
+                }
             }
             redirect();
         }
