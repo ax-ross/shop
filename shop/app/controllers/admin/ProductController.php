88ac19cde336551c8c02d90d568b12ce6345b139
ax-ross <ax-ross@yandex.ru>
2022-05-25 13:06:09 +0300

    implemented add action

--- shop/app/controllers/admin/ProductController.php
+++ shop/app/controllers/admin/ProductController.php
@@ -24,4 +24,32 @@ class ProductController extends AppController
         $this->setMeta("Админка :: {$title}");
         $this->set(compact('title', 'products', 'pagination', 'total'));
     }
+
+    public function addAction()
+    {
+        if (!empty($_POST)) {
+            if ($this->model->product_validate()) {
+                if ($this->model->save_product()) {
+                    $_SESSION['success'] = 'Товар добавлен';
+                } else {
+                    $_SESSION['errors'] = 'Ошибка добавления товара';
+                }
+
+            }
+            redirect();
+        }
+        $title = 'Новый товар';
+        $this->setMeta("Админка :: {$title}");
+        $this->set(compact('title'));
+    }
+
+    public function getDownloadAction()
+    {
+        $q = (string)$_GET['q'];
+        $downloads = $this->model->get_downloads($q);
+        echo json_encode($downloads);
+        die();
+    }
+
+
 }
