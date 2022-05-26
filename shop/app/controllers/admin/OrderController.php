0f84bb9bc60d256492421183b2d775288c6e34a8
ax-ross <ax-ross@yandex.ru>
2022-05-26 11:52:13 +0300

    implemented editing of order

--- shop/app/controllers/admin/OrderController.php
+++ shop/app/controllers/admin/OrderController.php
@@ -25,4 +25,26 @@ class OrderController extends AppController
         $this->setMeta("Админка :: {$title}");
         $this->set(compact('title', 'orders', 'pagination', 'total'));
     }
+
+    public function editAction()
+    {
+        $id = (int)$_GET['id'];
+        if (isset($_GET['status'])) {
+            $status = (int)$_GET['status'];
+            if ($this->model->change_status($id, $status)) {
+                $_SESSION['success'] = 'Статус заказа успешно изменён';
+            } else {
+                $_SESSION['errors'] = 'Не удалось изменить статус закза';
+            }
+        }
+
+        $order = $this->model->get_order($id);
+        if (!$order) {
+            throw new \Exception('Not found order', 404);
+        }
+
+        $title = "Заказ № {$id}";
+        $this->setMeta("Админка :: {$title}");
+        $this->set(compact('title', 'order'));
+    }
 }
